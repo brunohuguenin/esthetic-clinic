a90bc5b5361ecb6ad05a5ec719e54425f562120a
Brunohugueninn <brunohugueninn@gmail.com>
2024-01-30 20:53:14 -0300

    modificação da pasta js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import SlideNav from './slide.js';
+import SlideNav from './js/slide.js';
 const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
 slide.addControl('.custom-controls');
