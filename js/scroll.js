0ce3195191efc222bebef89e2fd1a3a85c40d3d3
Brunohugueninn <brunohugueninn@gmail.com>
2024-02-01 20:19:24 -0300

    criação da função de scroll suave

---
+++ js/scroll.js
@@ -0,0 +1,18 @@
+export default function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.menu-nav a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    const topo = section.offsetTop;
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
+}
--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 import SlideNav from './js/slide.js';
+import initScrollSuave from './js/scroll.js';
+
 const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
 slide.addControl('.custom-controls');
+initScrollSuave();
